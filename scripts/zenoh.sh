d91a2ddb70ef0870381b517a8888cd199f118a65
Pr0xximity999 <tomsomgamer999@gmail.com>
2025-12-17 13:39:36 +0100

    revamped services and scripts

--- scripts/zenoh.sh
+++ scripts/zenoh.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sleep 10
+# Remove if this is NOT the main router
+sleep 25
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-zenoh-bridge-ros2dds -e tcp/192.168.1.2:7447 -n /robotNUM
+zenoh-bridge-ros2dds #-e tcp/192.168.1.2:7447 -n /robotNUM
